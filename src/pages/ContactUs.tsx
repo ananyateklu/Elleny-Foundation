28399c1f2eecd4f6aec93c33f1d0bb2834333618
Fiseha Abebe <famarkfish79@gmail.com>
2023-06-08 22:06:13 +0300

    add contact us page

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -1,13 +1,70 @@
 // src/pages/Mission.tsx
 import React from 'react';
-import { Typography } from '@mui/material';
+import "../components/CSS/ContactUs.css";
 
-const Mission: React.FC = () => {
+const ContactUs: React.FC = () => {
   return (
-    <Typography variant="h4">
+    <div className='contact-main'>
+     <div className='contact-header'>
       Contact Us
-    </Typography>
+     </div>
+     <div className='contact-sub'>
+      
+      <div className='contact-form'>
+    <h1> For general inquiries, please contact us here:</h1> 
+      <div className='form'>
+      <form >
+                
+                <div className="name-inputs">
+                    <input
+                        type="text"
+                        placeholder="First Name"
+                        
+                        
+                        required
+                    />
+                    <input
+                        type="text"
+                        placeholder="Last Name"
+                        
+                        required
+                    />
+                </div>
+                <div className='email-phone-inputs'>
+                <input
+                    type="email"
+                    placeholder="Email Address"
+                    
+                    required
+                />
+                <input
+                    type="tel"
+                    placeholder="Phone"
+                    
+                    required
+                />
+                <textarea
+                                placeholder="Message"
+                                
+                                required
+                            ></textarea>
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+      </div>
+      </div>
+      <div className='contact-address'>
+        
+        <div className='address-rectangle'>
+          Locations
+        </div>
+      </div>
+     </div>
+     <div className='contact-map'>
+      Map Section
+     </div>
+      </div>
   );
 };
 
-export default Mission;
+export default ContactUs;
